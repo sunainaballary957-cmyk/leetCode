999589c79ff3ce105309269562fd1cc97a7e778b
sunaina B Ballary <sunainaballary957@gmail.com>
2026-07-03 20:04:35 +0530

    Time: 3 ms (18.46%), Space: 10.9 MB (95.35%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,32 @@
+int value(char ch)
+{
+    switch(ch)
+    {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s)
+{
+    int sum = 0;
+
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        int current = value(s[i]);
+        int next = value(s[i + 1]);
+
+        if(current < next)
+            sum -= current;
+        else
+            sum += current;
+    }
+
+    return sum;
+}
