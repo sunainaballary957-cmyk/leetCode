f57b4694318b0e457eeee46c71fe87f0c37376b2
sunaina B Ballary <sunainaballary957@gmail.com>
2026-07-07 23:05:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (69.59%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+
+bool isSubsequence(char *s, char *t)
+{
+    int i = 0;
+    int j = 0;
+
+    while(s[i] != '\0' && t[j] != '\0')
+    {
+        if(s[i] == t[j])
+        {
+            i++;
+        }
+
+        j++;
+    }
+
+    if(s[i] == '\0')
+        return true;
+
+    return false;
+}
