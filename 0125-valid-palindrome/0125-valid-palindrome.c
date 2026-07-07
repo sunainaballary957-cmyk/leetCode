0ec91e7368d863f1ddf793f80a70ac7d804850a3
sunaina B Ballary <sunainaballary957@gmail.com>
2026-07-07 22:40:13 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,42 +1,21 @@
 #include <stdbool.h>
+#include <ctype.h>
 #include <string.h>
 
-bool isPalindrome(char* s)
+bool isPalindrome(char *s)
 {
     int left = 0;
     int right = strlen(s) - 1;
 
     while(left < right)
     {
-        // Skip non-alphanumeric characters
-        while(left < right &&
-             !((s[left] >= 'A' && s[left] <= 'Z') ||
-               (s[left] >= 'a' && s[left] <= 'z') ||
-               (s[left] >= '0' && s[left] <= '9')))
-        {
+        while(left < right && !isalnum(s[left]))
             left++;
-        }
 
-        while(left < right &&
-             !((s[right] >= 'A' && s[right] <= 'Z') ||
-               (s[right] >= 'a' && s[right] <= 'z') ||
-               (s[right] >= '0' && s[right] <= '9')))
-        {
+        while(left < right && !isalnum(s[right]))
             right--;
-        }
 
-        // Convert uppercase to lowercase
-        char c1 = s[left];
-        char c2 = s[right];
-
-        if(c1 >= 'A' && c1 <= 'Z')
-            c1 = c1 + 32;
-
-        if(c2 >= 'A' && c2 <= 'Z')
-            c2 = c2 + 32;
-
-        // Compare
-        if(c1 != c2)
+        if(tolower(s[left]) != tolower(s[right]))
             return false;
 
         left++;
