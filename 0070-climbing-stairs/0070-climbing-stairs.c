0d833799dd855b5184918386abac3d765eb3d560
sunaina B Ballary <sunainaballary957@gmail.com>
2026-07-27 19:50:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (40.61%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,21 @@
+int climbStairs(int n)
+{
+    if(n == 1)
+        return 1;
+
+    if(n == 2)
+        return 2;
+
+    int first = 1;
+    int second = 2;
+    int current;
+
+    for(int i = 3; i <= n; i++)
+    {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return second;
+}
