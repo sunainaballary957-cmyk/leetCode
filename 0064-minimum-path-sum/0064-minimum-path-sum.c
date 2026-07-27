3257b0fdd97082c522d0cd06e3edad759e6dd9c5
sunaina B Ballary <sunainaballary957@gmail.com>
2026-07-27 19:59:13 +0530

    Time: 0 ms (100%), Space: 11.5 MB (21.7%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -0,0 +1,33 @@
+int min(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+
+int minPathSum(int** grid, int gridSize, int* gridColSize)
+{
+    int rows = gridSize;
+    int cols = gridColSize[0];
+
+    int dp[rows][cols];
+
+    dp[0][0] = grid[0][0];
+
+    // First row
+    for(int j = 1; j < cols; j++)
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+
+    // First column
+    for(int i = 1; i < rows; i++)
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+
+    // Remaining cells
+    for(int i = 1; i < rows; i++)
+    {
+        for(int j = 1; j < cols; j++)
+        {
+            dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[rows - 1][cols - 1];
+}
